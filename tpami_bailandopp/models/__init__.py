f864096e4c984542bcc56b1057868149eca29b04
J.Z. Zhou <79814775+jzho987@users.noreply.github.com>
2024-04-27 13:18:23 +1200

    fix imports

--- tpami_bailandopp/models/__init__.py
+++ tpami_bailandopp/models/__init__.py
@@ -1,9 +1,7 @@
-from .sep_vqvae import SepVQVAE
-from .sep_vqvae_root import SepVQVAER
-
 from .cross_cond_gpt2_music_window import CrossCondGPT2MW
 from .cross_cond_gpt2_music_window_ac import CrossCondGPT2MWAC
 from .sep_vqvae_root_mix import SepVQVAERmix
 from .up_down_half_reward import UpDownReward
 
-__all__ = ['SepVQVAER', 'SepVQVAE', 'SepVQVAERmix', 'CrossCondGPT2MWAC', 'CrossCondGPT2MW', ]
+# NOTE: only import the ones in tpami dir
+__all__ = ['SepVQVAERmix', 'CrossCondGPT2MWAC', 'CrossCondGPT2MW', ]
